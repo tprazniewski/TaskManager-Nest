c8658ddc7a44c048593f2308c19e637f8cdffd84
tprazniewski <104758997+tprazniewski@users.noreply.github.com>
2022-11-24 12:25:59 +0100

    Create DOCKER container for DB and establish connection. Install PG and typeorm packages

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,21 @@
 import { Module } from '@nestjs/common';
 import { TasksModule } from './tasks/tasks.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [TasksModule],
+  imports: [
+    TasksModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5439,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'task-management',
+      autoLoadEntities: true, //find entities and loadmthem for us
+      synchronize: true, //always keep the db schema
+    }),
+  ],
   controllers: [],
   providers: [],
 })
