8c90851c7bb422b28e5364fe88943cd9a977a6d3
tprazniewski <104758997+tprazniewski@users.noreply.github.com>
2022-11-24 19:03:46 +0100

    Create validation for credentials

--- src/auth/dto/auth-credentials-dto.ts
+++ src/auth/dto/auth-credentials-dto.ts
@@ -1,9 +1,24 @@
-import { IsNotEmpty } from 'class-validator';
+import {
+  IsNotEmpty,
+  IsString,
+  Matches,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
 
 export class AuthCredentialsDto {
+  @IsString()
   @IsNotEmpty()
+  @MinLength(4)
+  @MaxLength(30)
   username: string;
 
   @IsNotEmpty()
+  @IsString()
+  @MinLength(8)
+  @MaxLength(32)
+  @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+    message: 'Password is too weak',
+  })
   password: string;
 }
