b92a74cd7ed161993d42557916f94630798d8228
tprazniewski <104758997+tprazniewski@users.noreply.github.com>
2022-11-24 18:09:21 +0100

    Create signUp endpoint with controller service and repository

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { AuthCredentialsDto } from './dto/auth-credentials-dto';
+import { AuthService } from '../auth/auth.service';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private authService: AuthService) {}
+  @Post('/signup')
+  signUp(@Body() authCredentialsCto: AuthCredentialsDto): Promise<void> {
+    return this.authService.signUp(authCredentialsCto);
+  }
+}
--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,10 +1,21 @@
 import { Injectable } from '@nestjs/common';
 import { User } from './user.entity';
 import { Repository, DataSource } from 'typeorm';
+import { AuthCredentialsDto } from '../auth/dto/auth-credentials-dto';
 
 @Injectable()
 export class UserRepository extends Repository<User> {
   constructor(private dataSource: DataSource) {
     super(User, dataSource.createEntityManager());
+  }
+
+  async createUser(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+    const { username, password } = authCredentialsDto;
+
+    const user = this.create({
+      username,
+      password,
+    });
+    await this.save(user);
   }
 }
