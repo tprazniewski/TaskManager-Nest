f5d01faecafcfc61d1cc416ec731542e7cac51b0
tprazniewski <104758997+tprazniewski@users.noreply.github.com>
2022-11-24 14:32:17 +0100

    refactor the code to keep the db logic in the repository

--- src/tasks/task.repository.ts
+++ src/tasks/task.repository.ts
@@ -1,10 +1,21 @@
 import { Repository, DataSource } from 'typeorm';
 import { Injectable } from '@nestjs/common';
 import { Task } from './task.entity';
-
+import { CreateTaskDto } from '../tasks/dto/create-task-dto';
+import { TaskStatus } from '../tasks/task.status.enum';
 @Injectable()
 export class TaskRepository extends Repository<Task> {
   constructor(private dataSource: DataSource) {
     super(Task, dataSource.createEntityManager());
+  }
+  async createTask(createTaskDto: CreateTaskDto): Promise<Task> {
+    const { title, description } = createTaskDto;
+    const task = this.create({
+      title,
+      description,
+      status: TaskStatus.OPEN,
+    });
+    await this.save(task);
+    return task;
   }
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -22,15 +22,8 @@ export class TasksService {
     return found;
   }
 
-  async createTask(createTaskDto: CreateTaskDto): Promise<Task> {
-    const { title, description } = createTaskDto;
-    const task = this.taskRepository.create({
-      title,
-      description,
-      status: TaskStatus.OPEN,
-    });
-    await this.taskRepository.save(task);
-    return task;
+  createTask(createTaskDto: CreateTaskDto): Promise<Task> {
+    return this.taskRepository.createTask(createTaskDto);
   }
   // private tasks: ITask[] = [];
   // getTaskById(id: string): ITask {
